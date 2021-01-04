9bec032ed7ec50df285ee28c0eff6afe8a1a1d54
lezniak <ddezersl@gmail.com>
2021-01-05 00:29:34 +0100

    add item form

--- src/main/java/com/example/shop/controllers/itemController.java
+++ src/main/java/com/example/shop/controllers/itemController.java
@@ -1,9 +1,6 @@
 package com.example.shop.controllers;
 
-
 import com.example.shop.object.User;
-import com.example.shop.respositories.UserRepository;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,17 +8,24 @@ import org.springframework.web.bind.annotation.GetMapping;
 import javax.servlet.http.HttpSession;
 
 @Controller
-public class indexController {
-    @Autowired
-    private UserRepository userRepository;
+public class itemController {
 
-    @GetMapping(value = "/")
-    public String index(Model model, HttpSession session){
+    @GetMapping(value = "/items")
+    public String item(Model model, HttpSession session){
+
         User isExist= (User) session.getAttribute("loggedUser");
         if (null == isExist){
             return "index";
-        }else{
-            return "user_page";
         }
+        return "item";
+    }
+
+    @GetMapping(value = "/items/add")
+    public String itemAdd(Model model, HttpSession session){
+        User isExist= (User) session.getAttribute("loggedUser");
+        if (null == isExist){
+            return "index";
+        }
+        return "itemAdd";
     }
 }
---
+++ src/main/java/com/example/shop/object/item.java
@@ -0,0 +1,31 @@
+package com.example.shop.object;
+
+public class item {
+    private String itemName;
+    private String itemDesc;
+    private String itemQuality;
+
+    public String getItemName() {
+        return itemName;
+    }
+
+    public void setItemName(String itemName) {
+        this.itemName = itemName;
+    }
+
+    public String getItemDesc() {
+        return itemDesc;
+    }
+
+    public void setItemDesc(String itemDesc) {
+        this.itemDesc = itemDesc;
+    }
+
+    public String getItemQuality() {
+        return itemQuality;
+    }
+
+    public void setItemQuality(String itemQuality) {
+        this.itemQuality = itemQuality;
+    }
+}
